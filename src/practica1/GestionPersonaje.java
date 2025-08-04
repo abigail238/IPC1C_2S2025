e3b133961a1374b1ec5092d7cee38ddcb6df0e2a
abigail238 <3726005300115@ingenieria.usac.edu.gt>
2025-08-04 12:00:14 -0600

    mi primer comit

---
+++ src/practica1/GestionPersonaje.java
@@ -0,0 +1,11 @@
+package practica1;
+
+public class GestionPersonaje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("mi primera practica");
+
+	}
+
+}
